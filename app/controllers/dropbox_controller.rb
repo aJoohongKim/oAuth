156fbc58250b09fa3b939d65df3fe3d8d70b2543
Joohong Kim <joohong.kim@access-company.com>
2012-07-12 13:36:26 +0900

    display access tokens

--- app/controllers/dropbox_controller.rb
+++ app/controllers/dropbox_controller.rb
@@ -7,9 +7,9 @@ require 'dropbox_sdk'
 APP_KEY = "cx5zygh4caj53ua"
 APP_SECRET = "p9v34zcifm34i1q"
 ACCESS_TYPE = :dropbox #The two valid values here are :app_folder and :dropbox
-                          #The default is :app_folder, but your application might be
-                          #set to have full :dropbox access.  Check your app at
-                          #https://www.dropbox.com/developers/apps
+#The default is :app_folder, but your application might be
+#set to have full :dropbox access.  Check your app at
+#https://www.dropbox.com/developers/apps
 
 
 # Examples routes for config/routes.rb  (Rails 3)
@@ -18,47 +18,48 @@ ACCESS_TYPE = :dropbox #The two valid values here are :app_folder and :dropbox
 
 class DropboxController < ApplicationController
   def authorize
-        if not params[:oauth_token] then
-            dbsession = DropboxSession.new(APP_KEY, APP_SECRET)
+    if not params[:oauth_token] then
+      dbsession = DropboxSession.new(APP_KEY, APP_SECRET)
 
-            session[:dropbox_session] = dbsession.serialize #serialize and save this DropboxSession
+      session[:dropbox_session] = dbsession.serialize #serialize and save this DropboxSession
 
-            #pass to get_authorize_url a callback url that will return the user here
-            redirect_to dbsession.get_authorize_url url_for(:action => 'authorize')
-        else
-            # the user has returned from Dropbox
-            dbsession = DropboxSession.deserialize(session[:dropbox_session])
-            dbsession.get_access_token  #we've been authorized, so now request an access_token
-            session[:dropbox_session] = dbsession.serialize
-
-            redirect_to :action => 'upload'
-        end
-    end
-
-    def display_tokens
-      return redirect_to(:action => 'authorize') unless session[:dropbox_session]
-
+      #pass to get_authorize_url a callback url that will return the user here
+      redirect_to dbsession.get_authorize_url url_for(:action => 'authorize')
+    else
+      # the user has returned from Dropbox
       dbsession = DropboxSession.deserialize(session[:dropbox_session])
-      render :text => "#{dbsession.access_token()} what"
+      dbsession.get_access_token #we've been authorized, so now request an access_token
+      session[:dropbox_session] = dbsession.serialize
+
+      redirect_to :action => 'display_tokens'
     end
+  end
 
-    def upload
-        # Check if user has no dropbox session...re-direct them to authorize
-        return redirect_to(:action => 'authorize') unless session[:dropbox_session]
+  def display_tokens
+    return redirect_to(:action => 'authorize') unless session[:dropbox_session]
 
-        dbsession = DropboxSession.deserialize(session[:dropbox_session])
-        client = DropboxClient.new(dbsession, ACCESS_TYPE) #raise an exception if session not authorized
-        info = client.account_info # look up account information
+    dbsession = DropboxSession.deserialize(session[:dropbox_session])
+    access_token = dbsession.access_token
+    render :text => "accessKey=#{access_token.key} accessSecret=#{access_token.secret}"
+  end
 
-        if request.method != "POST"
-            # show a file upload page
-            render :inline =>
-                "#{info['email']} <br/><%= form_tag({:action => :upload}, :multipart => true) do %><%= file_field_tag 'file' %><%= submit_tag %><% end %>"
-            return
-        else
-            # upload the posted file to dropbox keeping the same name
-            resp = client.put_file(params[:file].original_filename, params[:file].read)
-            render :text => "Upload successful! File now at #{resp['path']}"
-        end
+  def upload
+    # Check if user has no dropbox session...re-direct them to authorize
+    return redirect_to(:action => 'authorize') unless session[:dropbox_session]
+
+    dbsession = DropboxSession.deserialize(session[:dropbox_session])
+    client = DropboxClient.new(dbsession, ACCESS_TYPE) #raise an exception if session not authorized
+    info = client.account_info # look up account information
+
+    if request.method != "POST"
+      # show a file upload page
+      render :inline =>
+                 "#{info['email']} <br/><%= form_tag({:action => :upload}, :multipart => true) do %><%= file_field_tag 'file' %><%= submit_tag %><% end %>"
+      return
+    else
+      # upload the posted file to dropbox keeping the same name
+      resp = client.put_file(params[:file].original_filename, params[:file].read)
+      render :text => "Upload successful! File now at #{resp['path']}"
     end
+  end
 end
